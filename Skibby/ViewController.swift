083ab87170d1a3472610b3fdf9088bed5e0b83ca
Charles Ferreira <charles.dev@live.com>
2018-02-22 17:55:00 -0300

    Solicita autorização para obter localização do usuário.

--- Skibby/ViewController.swift
+++ Skibby/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupLocationManager()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        locationManager.requestLocation()
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = Constants.location.distanceFilter
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    @IBAction func testGeofireTapped(_ sender: Any) {
+        
+    }
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.first
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Erro de localização: \(error)")
+    }
+}
+
+
+
 
