72badbd64cbf296210641c177babc2f61caa28c4
Charles Ferreira <charles.dev@live.com>
2018-02-22 15:51:34 -0300

    Initial Commit

---
+++ Skibby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skibby
+//
+//  Created by Charles Ferreira on 22/02/2018.
+//  Copyright © 2018 Charles Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
